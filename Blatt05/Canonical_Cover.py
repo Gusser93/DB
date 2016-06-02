48cbf2b788d4bb9830f0ad7e921e82ed848e9d1e
Markus Vieth <mavieth@students.uni-mainz.de>
2016-06-02 13:54:46 +0200

    bugfix

--- Blatt05/Canonical_Cover.py
+++ Blatt05/Canonical_Cover.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
 class R:
     def __init__(self, fds, attrs):
         self.fds = fds
         self.attrs = attrs
 
     def canonical_cover(self):
+        # Führe für jede FD α → β ∈ F die Linksreduktion durch
         for fd in self.fds:
             red_left(self, fd)
+        # Führe für jede (verbliebene) FD die Rechtsreduktion durch, also:
         for fd in self.fds:
             red_right(self, fd)
-
+        # Entferne die FDs der Form α ∅, die im 2. Schritt möglicherweise entstanden sind.
         self.remove_empty_fd()
+        # Fasse mittels der Vereinigungsregel FDs der Form α -> β1, ...,α -> βn zusammen, so dass α ->(β1 ∪ ... ∪ βn)
+        # verbleibt.
         self.union_dfs()
         print_fds(self.fds)
 
@@ -40,41 +45,62 @@ def print_fds(fds):
 
 
 def red_left(r, fd):
+    # Überprüfe für alle A ∈ α, ob A überflüssig ist, d.h., ob
     for a in fd.alpha:
         temp = fd.alpha.copy()
         temp.remove(a)
         closure = attr_closure(r.fds, temp)
+        # β ⊆ AttrHülle(F, α - A) gilt.
         if fd.beta <= closure:
+            # Falls dies der Fall ist, ersetze α → β durch (α - A) → β.
             fd.alpha = temp
 
 
 def red_right(r, fd):
+    # Überprüfe für alle B ∈ β, ob
     for b in fd.beta:
         temp = r.fds.copy()
         temp_beta = fd.beta.copy()
         temp_beta.remove(b)
         temp_fd = FD(fd.alpha, temp_beta)
-        if fd not in temp:
-            continue
+        # Werden diese Zeilen nicht kommentiert kommt ein anderes Ergebniss raus
+        #if fd not in temp:
+        #    print "nicht da"
+        #    print fd.alpha
+        #    print fd.beta
+        #    continue
+        #else:
+        #    print "da"
+        #    print fd.alpha
+        #    print fd.beta
         temp.remove(fd)
         temp.add(temp_fd)
         closure = attr_closure(temp, fd.alpha)
+        # B ∈ AttrHülle(F – (α → β) ∪ (α → (β − Β)), α ) gilt.
         if b in closure:
+            # Falls dies der Fall ist, ist B auf der rechten Seite überflüssig und kann eliminiert werden,
+            # d.h. ersetze α -> β durch α -> (β–B).
             r.fds = temp
+            fd = temp_fd
 
 
 def attr_closure(fds, attrs):
     change = True
+    # Erg := α
     erg = attrs
+    # While (Änderungen an Erg) do
     while change:
         change = False
+        # Foreach FD β → γ in F do
         for fd in fds:
+            # If β ⊆ Erg then Erg := Erg ∪ γ
             if fd.alpha <= erg:
                 temp = erg | fd.beta
+                # Test change
                 if len(erg ^ temp) != 0:
                     erg = temp
                     change = True
-
+    # Ausgabe α+ = Erg
     return erg
 
 if __name__ == "__main__":
